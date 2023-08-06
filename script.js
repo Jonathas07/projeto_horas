8a6ed38b250e0f8d2b0734aab54c7e20059bb4fc
Jonathas Souza <130953681+Jonathas07@users.noreply.github.com>
2023-08-05 21:14:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function carregar(){
+    let timereal = new Date()
+    let horas = timereal.getHours()
+    // let horas = 01
+    var textoHora = window.document.getElementById('textoHora')
+    var imagem = window.document.getElementById('foto')
+    textoHora.innerHTML = `Agora São ${horas} horas`
+    if(horas<12&&horas>=6){
+        window.document.body.style.backgroundColor = 'rgb(216, 204, 69)'
+        imagem.src= 'https://images.pexels.com/photos/955656/pexels-photo-955656.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1 '
+    }else if(horas>=12&&horas<18){
+        window.document.body.style.backgroundColor = 'rgba(218, 120, 75, 0.838)'
+        imagem.src = 'https://images.pexels.com/photos/3347244/pexels-photo-3347244.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }else if(horas>=18&&horas<24){
+        window.document.body.style.backgroundColor = 'rgba(1, 5, 52, 0.661)'
+        imagem.src = 'https://images.pexels.com/photos/3075993/pexels-photo-3075993.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }else{
+        window.document.body.style.backgroundColor = 'rgba(1, 5, 52, 0.661)'
+        imagem.src = 'https://img.freepik.com/fotos-premium/estrelas-da-via-lactea-no-ceu-noturno-espaco-no-fundo-da-madrugada-da-manha_445280-3218.jpg'
+    }
+}
